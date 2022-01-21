d63bf4ffaffc31c1733ef081b57eeeb24e9dc4b3
Mirek <miro.zaprazny8@gmail.com>
2022-01-21 13:51:54 +0100

    Offers

--- backend/controllers/OffersController.php
+++ backend/controllers/OffersController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace backend\controllers;
 
 use common\models\documents\templatedocuments\PdfTemplateDocument;
@@ -32,7 +33,7 @@ class OffersController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index',[
+        return $this->render('index', [
             'offers' => $this->getOfferList(),
             'senders' => $this->getSenders()
         ]);
@@ -42,13 +43,13 @@ class OffersController extends Controller
     {
         if (Yii::$app->request->isPost) {
             $data = Yii::$app->request->post('Data');
-            $template = Template::findOne(['id'=>474]);
+            $template = Template::findOne(['id' => 474]);
             $doc = new PdfTemplateDocument();
             $doc->setTemplateContent($template->content);
             unset($template);
             $offerIds = $this->getOfferIds($data['recips']);
             $doc->setTemplateData([
-                'offer' => Offer::find()->where(['in','id', $offerIds])->all(),
+                'offer' => Offer::find()->where(['in', 'id', $offerIds])->all(),
                 'input.miesto_podpisu_hu'  => $data['miesto_podpisu']['HU'],
                 'input.miesto_podpisu_sk'  => $data['miesto_podpisu']['SK'],
                 'input.datum_podpisu_hu'   => (new \DateTimeImmutable($data['datum_podpisu']['HU']))->format('Y.m.d'),
@@ -62,10 +63,10 @@ class OffersController extends Controller
             ]);
             $doc->create();
             $doc->downloadFile();
-            $sql = "UPDATE offer SET informed=1 WHERE id in (". implode(",",$offerIds) .")";
+            $sql = "UPDATE offer SET informed=1 WHERE id in (" . implode(",", $offerIds) . ")";
             Yii::$app->db->createCommand($sql)->execute();
         }
-        $this->redirect('/offers/index',200);
+        $this->redirect('/offers/index', 200);
     }
 
     public function actionImport()
@@ -73,61 +74,78 @@ class OffersController extends Controller
         if (Yii::$app->request->isPost) {
             $data = Yii::$app->request->post('Import');
             if (empty($_FILES) || $_FILES['DataFile']['type'] != 'application/vnd.ms-excel') {
-                Yii::$app->session->setFlash('error',Yii::t('app','Nahratý súbor má zlý formát! Nahrajte CSV súbor (UTF8)!'));
+                Yii::$app->session->setFlash('error', Yii::t('app', 'Nahratý súbor má zlý formát! Nahrajte CSV súbor (UTF8)!'));
             } else {
                 $result = $this->processCsvInput($_FILES['DataFile']['tmp_name'], $data['delimiter']);
-                $sql = "insert into offer (`propertyType`,`orderNumber`,`gender`,`name`,`lastName`,`maidenName`,".
-                    "`birthDate`,`ownerAddress`,`ownerTown`,`coOwnership`,`acquisitionTitle`,`encumbrance`,".
+                $sql = "insert into offer (`propertyType`,`orderNumber`,`gender`,`name`,`lastName`,`maidenName`," .
+                    "`birthDate`,`ownerAddress`,`ownerTown`,`coOwnership`,`acquisitionTitle`,`encumbrance`," .
                     "`registerNumber`,`parcelNumber`,`ownershipDocumentNumber`,`propertyAddress`)  values %s";
                 $rows = [];
-                foreach($result as $item) {
-                    $rows[] = "(".implode(',',[
-                            $data['type'],
-                            $item['orderNumber'] != '' ? $item['orderNumber'] : 'null' ,
-                            $item['gender'],
-                            "'{$item['name']}'",
-                            "'{$item['lastName']}'",
-                            "'{$item['maidenName']}'",
-                            "'{$item['birthDate']}'",
-                            "'{$item['ownerAddress']}'",
-                            "'{$item['ownerTown']}'",
-                            "'{$item['coOwnership']}'",
-                            "'{$item['acquisitionTitle']}'",
-                            "'{$item['encumbrance']}'",
-                            "'{$item['registerNumber']}'",
-                            "'{$item['parcelNumber']}'",
-                            "'{$item['ownershipDocumentNumber']}'",
-                            "'{$item['propertyAddress']}'"
-                        ]).")";
+                foreach ($result as $item) {
+                    $rows[] = "(" . implode(',', [
+                        $data['type'],
+                        $item['orderNumber'] != '' ? $item['orderNumber'] : 'null',
+                        $item['gender'],
+                        "'{$item['name']}'",
+                        "'{$item['lastName']}'",
+                        "'{$item['maidenName']}'",
+                        "'{$item['birthDate']}'",
+                        "'{$item['ownerAddress']}'",
+                        "'{$item['ownerTown']}'",
+                        "'{$item['coOwnership']}'",
+                        "'{$item['acquisitionTitle']}'",
+                        "'{$item['encumbrance']}'",
+                        "'{$item['registerNumber']}'",
+                        "'{$item['parcelNumber']}'",
+                        "'{$item['ownershipDocumentNumber']}'",
+                        "'{$item['propertyAddress']}'"
+                    ]) . ")";
                 }
-                $sql = sprintf($sql, implode(',',$rows));
+                $sql = sprintf($sql, implode(',', $rows));
                 $tr = Yii::$app->db->beginTransaction();
-                try{
+                try {
                     Yii::$app->db->createCommand($sql)->execute();
                     $tr->commit();
-                    Yii::$app->session->setFlash('success', Yii::t('app','Údaje boli úspešne nahraté'));
+                    Yii::$app->session->setFlash('success', Yii::t('app', 'Údaje boli úspešne nahraté'));
                     return $this->redirect(Url::to(['/offers']));
-                } catch(\Exception $e) {
+                } catch (\Exception $e) {
                     $tr->rollBack();
-                    Yii::$app->session->setFlash('error',$e->getMessage());
+                    Yii::$app->session->setFlash('error', $e->getMessage());
                 }
             }
         }
 
-        return $this->render('import',[
-        ]);
+        return $this->render('import', []);
     }
+
+
 
     public function actionEdit(int $on)
     {
-        return $this->render('edit');
+        $offers = Offer::find()->where(['=', 'orderNumber', $on])->all();
+
+        if (Yii::$app->request->isPost) {
+            $data = Yii::$app->request->post('data');
+            foreach ($data as $id => $item) {
+                $offer = Offer::findOne(['id' => $id]);
+                foreach ($item as $col => $val) {
+                    $offer->$col = $val;
+                }
+                $offer->save();
+            }
+            return $this->redirect('index');
+        }
+
+        return $this->render('edit', [
+            'offers' => $offers
+        ]);
     }
 
     private function getOfferIds(string $ids): array
     {
-        $ids = explode(',',trim($ids));
+        $ids = explode(',', trim($ids));
         $returns = [];
-        foreach($ids as $id) {
+        foreach ($ids as $id) {
             $this->processOfferId($id, $returns);
         }
         return $returns;
@@ -138,9 +156,9 @@ class OffersController extends Controller
         $orderNumber = trim($orderNumber);
         if (is_numeric($orderNumber)) {
             $returns = array_merge($returns, [$this->getOfferIdFromOrderNumber($orderNumber)]);
-        } elseif(strpos($orderNumber,'-') !== false) {
-            list($start,$end) = explode('-',$orderNumber);
-            for($i=$start;$i<=$end;$i++) {
+        } elseif (strpos($orderNumber, '-') !== false) {
+            list($start, $end) = explode('-', $orderNumber);
+            for ($i = $start; $i <= $end; $i++) {
                 $returns = array_merge($returns, [$this->getOfferIdFromOrderNumber($i)]);
             }
         }
@@ -148,7 +166,7 @@ class OffersController extends Controller
 
     private function getOfferIdFromOrderNumber(int $orderNumber): int
     {
-        $result = Offer::find()->where(['=','orderNumber',$orderNumber])->all();
+        $result = Offer::find()->where(['=', 'orderNumber', $orderNumber])->all();
         return ($result[0])->id;
     }
 
@@ -219,7 +237,7 @@ class OffersController extends Controller
         // remove BOM string
         //
         $content = file_get_contents($csvFile);
-        file_put_contents($csvFile, str_replace("\xEF\xBB\xBF",'', $content));
+        file_put_contents($csvFile, str_replace("\xEF\xBB\xBF", '', $content));
 
         $result = [];
         if (($hFile = fopen($csvFile, "r")) !== FALSE) {
@@ -258,11 +276,11 @@ class OffersController extends Controller
         return trim($str);
     }
 
-    private function convertDate(string $date, string $dateFormat='Y-m-d'): string
+    private function convertDate(string $date, string $dateFormat = 'Y-m-d'): string
     {
         $date = $this->sanitizeString($date);
-        $date = str_replace('/','',$date);
-        $date = str_replace('?','',$date);
+        $date = str_replace('/', '', $date);
+        $date = str_replace('?', '', $date);
         if (strlen($date) == 0) {
             return '';
         }
@@ -271,7 +289,6 @@ class OffersController extends Controller
 
     private function convertCoOwnerShip(string $ownerShip): string
     {
-        return str_replace('.','/',$this->sanitizeString($ownerShip));
+        return str_replace('.', '/', $this->sanitizeString($ownerShip));
     }
-
 }
--- backend/views/offers/index.php
+++ backend/views/offers/index.php
@@ -1,12 +1,13 @@
 <?php
+
 use yii\helpers\Url;
 use backend\assets\RealAsset;
 
-$this->title = Yii::t('app','Garáže');
-$this->registerJSFile('@web/assets/node_modules/datatables/datatables.min.js',['depends'=>RealAsset::class]);
-$this->registerCSSFile('@web/assets/node_modules/datatables/media/css/dataTables.bootstrap4.css',['depends'=>RealAsset::class]);
-$this->registerJSFile('@web/assets/node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js', ['depends'=>RealAsset::class]);
-$this->registerCSSFile('@web/assets/node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.css',['depends'=>RealAsset::class]);
+$this->title = Yii::t('app', 'Garáže');
+$this->registerJSFile('@web/assets/node_modules/datatables/datatables.min.js', ['depends' => RealAsset::class]);
+$this->registerCSSFile('@web/assets/node_modules/datatables/media/css/dataTables.bootstrap4.css', ['depends' => RealAsset::class]);
+$this->registerJSFile('@web/assets/node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js', ['depends' => RealAsset::class]);
+$this->registerCSSFile('@web/assets/node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.css', ['depends' => RealAsset::class]);
 
 ?>
 
@@ -17,7 +18,7 @@ $this->registerCSSFile('@web/assets/node_modules/bootstrap-tagsinput/dist/bootst
         </div>
         <div class="col-md-2 align-self-center text-right">
             <a class="btn btn-info d-none d-lg-block m-l-15 text-white" href="<?= Url::to(['/offers/import']) ?>">
-                <i class="mdi mdi-cloud-upload m-r-5"></i>&nbsp;<?= Yii::t('app','Import dát'); ?>
+                <i class="mdi mdi-cloud-upload m-r-5"></i>&nbsp;<?= Yii::t('app', 'Import dát'); ?>
             </a>
         </div>
     </div>
@@ -31,20 +32,20 @@ $this->registerCSSFile('@web/assets/node_modules/bootstrap-tagsinput/dist/bootst
                     <?php
                     //TODO: meg ellenorzeseket ide rakni
                     ?>
-                    <form method="post" action="<?= Url::to(['generate-list'])?>">
+                    <form method="post" action="<?= Url::to(['generate-list']) ?>">
                         <input type="hidden" name="<?= Yii::$app->request->csrfParam ?>" value="<?= Yii::$app->request->getCsrfToken() ?>">
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
-                                    <label class="form-label"><?= Yii::t('app','Odosielateľ'); ?></label>
+                                    <label class="form-label"><?= Yii::t('app', 'Odosielateľ'); ?></label>
                                     <select class="form-control form-select" id="SelectSender">
-                                        <option value=""><?= Yii::t('app','Zvoľte si odosielateľa'); ?></option>
+                                        <option value=""><?= Yii::t('app', 'Zvoľte si odosielateľa'); ?></option>
                                         <?php
-                                            foreach($senders as $sender) {
+                                        foreach ($senders as $sender) {
                                         ?>
-                                                <option value='<?= json_encode($sender)?>'><?= $sender['name'] ?></option>
-                                        <?php        
-                                            }
+                                            <option value='<?= json_encode($sender) ?>'><?= $sender['name'] ?></option>
+                                        <?php
+                                        }
                                         ?>
                                     </select>
                                 </div>
@@ -52,86 +53,111 @@ $this->registerCSSFile('@web/assets/node_modules/bootstrap-tagsinput/dist/bootst
                         </div>
                         <div class="row">
                             <?php
-                            $langs = ['HU','SK'];
-                            foreach($langs as $lang) {
+                            $langs = ['HU', 'SK'];
+                            foreach ($langs as $lang) {
                             ?>
                                 <div class="col-md-4">
-                                    <h4 class="card-title mb-2"><?= Yii::t('app',"Dáta pre {$lang} email"); ?></h4>
+                                    <h4 class="card-title mb-2"><?= Yii::t('app', "Dáta pre {$lang} email"); ?></h4>
                                     <div class="form-group">
-                                        <label class="form-label mt-2"><?= Yii::t('app','Telefón odosielateľa'); ?></label>
-                                        <input type="text" name="Data[buyer_phone][<?=$lang?>]" class="form-control buyer_phone">
-                                        <label class="form-label mt-2"><?= Yii::t('app','Email odosielateľa'); ?></label>
-                                        <input type="text" name="Data[buyer_email][<?=$lang?>]" class="form-control buyer_email">
-                                        <label class="form-label mt-2"><?= Yii::t('app','Meno odosielateľa'); ?></label>
-                                        <input type="text" name="Data[buyer_name][<?=$lang?>]" class="form-control buyer_name">
-                                        <label class="form-label mt-2"><?= Yii::t('app','Miesto podpisu'); ?></label>
-                                        <input type="text" name="Data[miesto_podpisu][<?=$lang?>]" class="form-control miesto_podpisu">
-                                        <label class="form-label mt-2"><?= Yii::t('app','Dátum podpisu'); ?></label>
-                                        <input type="date" name="Data[datum_podpisu][<?=$lang?>]" class="form-control"  value="<?php echo date('Y-m-d'); ?>">
+                                        <label class="form-label mt-2"><?= Yii::t('app', 'Telefón odosielateľa'); ?></label>
+                                        <input type="text" name="Data[buyer_phone][<?= $lang ?>]" class="form-control buyer_phone">
+                                        <label class="form-label mt-2"><?= Yii::t('app', 'Email odosielateľa'); ?></label>
+                                        <input type="text" name="Data[buyer_email][<?= $lang ?>]" class="form-control buyer_email">
+                                        <label class="form-label mt-2"><?= Yii::t('app', 'Meno odosielateľa'); ?></label>
+                                        <input type="text" name="Data[buyer_name][<?= $lang ?>]" class="form-control buyer_name">
+                                        <label class="form-label mt-2"><?= Yii::t('app', 'Miesto podpisu'); ?></label>
+                                        <input type="text" name="Data[miesto_podpisu][<?= $lang ?>]" class="form-control miesto_podpisu">
+                                        <label class="form-label mt-2"><?= Yii::t('app', 'Dátum podpisu'); ?></label>
+                                        <input type="date" name="Data[datum_podpisu][<?= $lang ?>]" class="form-control" value="<?php echo date('Y-m-d'); ?>">
                                     </div>
                                 </div>
                             <?php
-                                }
+                            }
                             ?>
                         </div>
-                        <h5 class="card-title"><?= Yii::t('app','Príjemcovia'); ?></h5>
+                        <h5 class="card-title"><?= Yii::t('app', 'Príjemcovia'); ?></h5>
                         <div class="row">
                             <div class="col-md-12">
                                 <input type="text" class="form-control" name="Data[recips]">
-                                <span class="text-muted font-12"><?= Yii::t('app','Pr. 1-10,15,16,23-27'); ?></span>
+                                <span class="text-muted font-12"><?= Yii::t('app', 'Pr. 1-10,15,16,23-27'); ?></span>
                             </div>
                         </div>
-                        <button class="btn btn-info text-white mt-3" type="submit"><?= Yii::t('app','Generovať'); ?></button>
+                        <button class="btn btn-info text-white mt-3" type="submit"><?= Yii::t('app', 'Generovať'); ?></button>
                     </form>
                     <div class="table-responsive m-t-20">
                         <table class="table table-bordered table-striped table-sm dattable">
                             <thead>
-                            <tr>
-                                <th><?= Yii::t('app','Por. č.'); ?></th>
-                                <th><?= Yii::t('app','Majiteľ'); ?></th>
-                                <th><?= Yii::t('app','Dát. narod.'); ?></th>
-                                <th><?= Yii::t('app','Ulica'); ?></th>
-                                <th><?= Yii::t('app','Mesto'); ?></th>
-                                <th><?= Yii::t('app','Spoluvl. pod.'); ?></th>
-                                <th><?= Yii::t('app','Titul nadobud.'); ?></th>
-                                <th><?= Yii::t('app','Ťarchy'); ?></th>
-                                <th><?= Yii::t('app','Súp.č.'); ?></th>
-                                <th><?= Yii::t('app','Č.parcely') ?></th>
-                                <th><?= Yii::t('app','LV') ?></th>
-                                <th><?= Yii::t('app','Miesto') ?></th>
-                                <th><?= Yii::t('app','Akcie'); ?></th>
-                            </tr>
+                                <tr>
+                                    <th><?= Yii::t('app', 'Por. č.'); ?></th>
+                                    <th><?= Yii::t('app', 'Majiteľ'); ?></th>
+                                    <th><?= Yii::t('app', 'Informovanný'); ?></th>
+                                    <th><?= Yii::t('app', 'Dát. narod.'); ?></th>
+                                    <th><?= Yii::t('app', 'Ulica'); ?></th>
+                                    <th><?= Yii::t('app', 'Mesto'); ?></th>
+                                    <th><?= Yii::t('app', 'Spoluvl. pod.'); ?></th>
+                                    <th><?= Yii::t('app', 'Titul nadobud.'); ?></th>
+                                    <th><?= Yii::t('app', 'Ťarchy'); ?></th>
+                                    <th><?= Yii::t('app', 'Súp.č.'); ?></th>
+                                    <th><?= Yii::t('app', 'Č.parcely') ?></th>
+                                    <th><?= Yii::t('app', 'LV') ?></th>
+                                    <th><?= Yii::t('app', 'Miesto') ?></th>
+                                    <th><?= Yii::t('app', 'Akcie'); ?></th>
+                                </tr>
                             </thead>
                             <tbody>
-                            <?php
-                            foreach($offers as $offer) {
-                                ?>
-                                <tr>
-                                    <td>
-                                        <?= $offer['orderNumber'] ?>
-                                    </td>
-                                    <td>
-                                        <?php
-                                        echo $offer['name']
-                                        ?>
-                                    </td>
-                                    <td><?= $offer['birthDate'] ?></td>
-                                    <td><?= $offer['ownerAddress'] ?></td>
-                                    <td><?= $offer['ownerTown'] ?></td>
-                                    <td><?= $offer['coOwnership'] ?></td>
-                                    <td><?= $offer['acquisitionTitle'] ?></td>
-                                    <td><?= $offer['encumbrance'] ?></td>
-                                    <td><?= $offer['registerNumber'] ?></td>
-                                    <td><?= $offer['parcelNumber'] ?></td>
-                                    <td><?= $offer['ownershipDocumentNumber'] ?></td>
-                                    <td><?= $offer['propertyAddress'] ?></td>
-                                    <td>
-                                        <a href="<?= Url::to(['edit','on'=>$offer['orderNumber']]) ?>" title="<?= Yii::t('app','Upraviť'); ?>" style="color: black"><i class="fas fa-pencil-alt"></i></a>
-                                    </td>
-                                </tr>
                                 <?php
-                            }
-                            ?>
+                                foreach ($offers as $offer) {
+                                ?>
+                                    <tr>
+                                        <td>
+                                            <?= $offer['orderNumber'] ?>
+                                        </td>
+                                        <td>
+                                            <?php
+                                            echo $offer['name']
+                                            ?>
+                                        </td>
+                                        <?php
+
+                                        if ($offer['informed'] == 1) {
+                                        ?>
+                                            <td>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                                                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                                                </svg>
+                                            </td>
+                                        <?php
+                                        }
+
+                                        ?>
+                                        <?php
+                                        if ($offer['informed'] == 0) {
+                                        ?>
+                                            <td>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                                </svg>
+                                            </td>
+                                        <?php
+                                        }
+                                        ?>
+                                        <td><?= $offer['birthDate'] ?></td>
+                                        <td><?= $offer['ownerAddress'] ?></td>
+                                        <td><?= $offer['ownerTown'] ?></td>
+                                        <td><?= $offer['coOwnership'] ?></td>
+                                        <td><?= $offer['acquisitionTitle'] ?></td>
+                                        <td><?= $offer['encumbrance'] ?></td>
+                                        <td><?= $offer['registerNumber'] ?></td>
+                                        <td><?= $offer['parcelNumber'] ?></td>
+                                        <td><?= $offer['ownershipDocumentNumber'] ?></td>
+                                        <td><?= $offer['propertyAddress'] ?></td>
+                                        <td>
+                                            <a href="<?= Url::to(['edit', 'on' => $offer['orderNumber']]) ?>" title="<?= Yii::t('app', 'Upraviť'); ?>" style="color: black"><i class="fas fa-pencil-alt"></i></a>
+                                        </td>
+                                    </tr>
+                                <?php
+                                }
+                                ?>
                             </tbody>
                         </table>
                     </div>
